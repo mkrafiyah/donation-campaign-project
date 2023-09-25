f8a766a2f8841fda6fd6918f7471e8cbe46de89f
mkrafiyah <rafiyah.mahbub@gmail.com>
2023-09-25 12:04:58 +0400

    toast notification added

--- src/Components/CardDetails/CardDetails.jsx
+++ src/Components/CardDetails/CardDetails.jsx
@@ -1,4 +1,6 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const CardDetails = () => {
@@ -7,6 +9,10 @@ const CardDetails = () => {
     const idInt = parseInt(id);
     const card = cardDetail.find(card => card.id === idInt);
     console.log(card)
+
+    const handleClick = () =>{
+        toast("Thank You!Your donation is successful");
+    }
     return (
         // <div className="flex justify-center">
         //     <div className="card w-96 bg-base-100 shadow-xl">
@@ -21,12 +27,13 @@ const CardDetails = () => {
         <div className="max-w-screen-md flex justify-center mx-auto">
             <div>
             <div><img className="w-full" src={card.image} alt="" /></div>
-            <div className="py-8 bg-slate-950  opacity-70"><button className="bg-red-500 btn">donate</button></div>
+            <div className="py-8 bg-slate-950  opacity-70"><button onClick={handleClick} className="bg-red-500 btn">donate ${card.price}</button></div>
             <div>
                 <h2 className="text-3xl">{card.title}</h2>
                 <p>{card.description}</p>
             </div>
             </div>
+            <ToastContainer />
         </div>
     );
 };
